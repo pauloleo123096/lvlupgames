dd0506a1c66aac984022dc5c299d6e0ee661af1c
pauloleo123096 <43679500+pauloleo123096@users.noreply.github.com>
2025-07-22 22:42:40 +0800

    Delete game.js

--- game.js
+++
@@ -1,6 +0,0 @@
-// Simple placeholder JS for the floppy bird clone
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = '#000';
-ctx.font = '30px Arial';
-ctx.fillText('Floppy Bird Clone!', 20, 240);
